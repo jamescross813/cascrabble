e7a563ae7b70984c92a82cb35a2fd1fa24452d47
jamescross813 <crossjames813@gmail.com>
2023-05-11 22:05:03 -0500

    added in blanks

--- script.py
+++ script.py
@@ -2,5 +2,5 @@ letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N",
 points = [1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 4, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10]
 
 letters_to_points = {key:value for key, value in zip(letters, points)}
-
+letters_to_points[" "]=0
 print(letters_to_points)
